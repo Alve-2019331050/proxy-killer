8db8fd5feb8ab9e27a6aefa5018dbbcac6b11958
Md.Shibly Rahman Alve <alverohman2000@gmail.com>
2023-03-06 15:28:46 +0600

    changed ui of start class

--- lib/screens/home/teacher/start_class.dart
+++ lib/screens/home/teacher/start_class.dart
@@ -15,6 +15,7 @@ class startClass extends StatefulWidget {
 class _startClassState extends State<startClass> {
   TimeOfDay _startTimeOfDay = new TimeOfDay(hour: 0, minute: 0);
   TimeOfDay _endTimeOfDay = new TimeOfDay(hour: 0, minute: 0);
+  DateTime _dateTime = DateTime.now();
   String dropDownValue = 'Select Course';
 
   Widget buildDropDown(dynamic data){
@@ -61,10 +62,10 @@ class _startClassState extends State<startClass> {
     // ),
     dayPeriodBorderSide: const BorderSide(color: Colors.black26, width: 2),
     // dayPeriodColor: Colors.blue[900],
-    // shape: const RoundedRectangleBorder(
-    //   borderRadius: BorderRadius.all(Radius.circular(8)),
-    //   side: BorderSide(color: Color(0xff5e83ba), width: 4),
-    // ),
+    shape: const RoundedRectangleBorder(
+      borderRadius: BorderRadius.all(Radius.circular(15)),
+      // side: BorderSide(color: Color(0xff5e83ba), width: 4),
+    ),
     // dayPeriodTextColor: Colors.white,
     dayPeriodShape: const RoundedRectangleBorder(
       borderRadius: BorderRadius.all(Radius.circular(8)),
@@ -78,7 +79,7 @@ class _startClassState extends State<startClass> {
     dialBackgroundColor: Colors.black26,
     hourMinuteTextStyle: const TextStyle(fontSize: 45, fontWeight: FontWeight.bold),
     dayPeriodTextStyle: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-    entryModeIconColor: Colors.black87,
+    entryModeIconColor: Colors.blue.shade900,
   );
 
   void _showTimePickerEnd(){
@@ -92,9 +93,9 @@ class _startClassState extends State<startClass> {
               timePickerTheme: _timePickerTheme,
               textButtonTheme: TextButtonThemeData(
                 style: ButtonStyle(
-                  backgroundColor: MaterialStateColor.resolveWith((states) => Colors.black87),
+                  backgroundColor: MaterialStateColor.resolveWith((states) => Colors.blue.shade900),
                   foregroundColor: MaterialStateColor.resolveWith((states) => Colors.white),
-                  overlayColor: MaterialStateColor.resolveWith((states) => Colors.black87),
+                  overlayColor: MaterialStateColor.resolveWith((states) => Colors.blue.shade900),
                 ),
               ),
             ),
@@ -119,9 +120,9 @@ class _startClassState extends State<startClass> {
             timePickerTheme: _timePickerTheme,
             textButtonTheme: TextButtonThemeData(
               style: ButtonStyle(
-                backgroundColor: MaterialStateColor.resolveWith((states) => Colors.black87),
+                backgroundColor: MaterialStateColor.resolveWith((states) => Colors.blue.shade900),
                 foregroundColor: MaterialStateColor.resolveWith((states) => Colors.white),
-                overlayColor: MaterialStateColor.resolveWith((states) => Colors.black87),
+                overlayColor: MaterialStateColor.resolveWith((states) => Colors.blue.shade900),
               ),
             ),
           ),
@@ -131,6 +132,42 @@ class _startClassState extends State<startClass> {
     ).then((value){
       setState(() {
         _startTimeOfDay = value!;
+      });
+    });
+  }
+
+  void _showDatePicker(){
+    showDatePicker(
+        context: context,
+        initialDate: DateTime.now(),
+        firstDate: DateTime(2000),
+        lastDate: DateTime(2030),
+        builder: (context, child) {
+          return Theme(
+            data: Theme.of(context).copyWith(
+              dialogTheme: DialogTheme(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+              ),
+              colorScheme: ColorScheme.light(
+                primary: Color(0xFF001a33), // header background color
+                onPrimary: Colors.white, // header text color
+                onSurface: Color(0xFF001a33), // body text color
+              ),
+              dialogBackgroundColor: Colors.blue[50],
+              textButtonTheme: TextButtonThemeData(
+                style: TextButton.styleFrom(
+                  foregroundColor: Color(0xFF001a33), // button text color
+                ),
+              ),
+            ),
+            child: child!,
+          );
+        },
+    ).then((value){
+      setState(() {
+        _dateTime = value!;
       });
     });
   }
@@ -148,122 +185,165 @@ class _startClassState extends State<startClass> {
         elevation: 0,
       ),
       body: Padding(
-        padding: EdgeInsets.only(left:30,top:40),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(height: 15,),
-            Text(
-              'It\'s time to Start\na new Class!',
-              style: TextStyle(
-                  fontSize: 30,
-                  color:Color(0xFF001a33),
-              ),
-            ),
-            SizedBox(height:30),
-            Row(
-              children: [
-                Text(
-                  'Course :',
-                  style: TextStyle(
-                    fontSize: 25,
+        padding: EdgeInsets.only(left:30,top:25),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 15,),
+              Text(
+                'It\'s time to Start\na new Class!',
+                style: TextStyle(
+                    fontSize: 30,
                     color:Color(0xFF001a33),
-                  ),
                 ),
-                SizedBox(width: 10,),
-                StreamBuilder(
-                  stream: FirebaseFirestore.instance.collection('users').doc(uid).snapshots(),
-                  builder: (context,AsyncSnapshot<DocumentSnapshot> snapshot){
-                    if(snapshot.hasData){
-                      var object = snapshot.data!;
-                      String name = object['name'];
-                      return StreamBuilder(
-                        stream: FirebaseFirestore.instance.collection('courses').where('teacher',isEqualTo: name).snapshots(),
-                        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot){
-                          if(snapshot.hasData){
-                            return buildDropDown(snapshot.data);
-                          }
-                          else{
-                            return Container();
-                          }
-                        },
-                      );
+              ),
+              SizedBox(height:30),
+              Row(
+                children: [
+                  Text(
+                    'Course :',
+                    style: TextStyle(
+                      fontSize: 25,
+                      color:Color(0xFF001a33),
+                    ),
+                  ),
+                  SizedBox(width: 10,),
+                  StreamBuilder(
+                    stream: FirebaseFirestore.instance.collection('users').doc(uid).snapshots(),
+                    builder: (context,AsyncSnapshot<DocumentSnapshot> snapshot){
+                      if(snapshot.hasData){
+                        var object = snapshot.data!;
+                        String name = object['name'];
+                        return StreamBuilder(
+                          stream: FirebaseFirestore.instance.collection('courses').where('teacher',isEqualTo: name).snapshots(),
+                          builder: (context, AsyncSnapshot<QuerySnapshot> snapshot){
+                            if(snapshot.hasData){
+                              return buildDropDown(snapshot.data);
+                            }
+                            else{
+                              return Container();
+                            }
+                          },
+                        );
+                      }
+                      else return const Material(child: Center(child: CircularProgressIndicator(),),);
                     }
-                    else return const Material(child: Center(child: CircularProgressIndicator(),),);
-                  }
-                ),
-              ],
-            ),
-            SizedBox(height:60),
-            Row(
-              children:[
-                Text(
-                  'Pick Starting time :',
-                  style: TextStyle(
-                    fontSize: 25,
-                    color: Color(0xFF001a33),
                   ),
-                ),
-                SizedBox(width:10),
-                ElevatedButton(
-                    onPressed: _showTimePickerStart,
+                ],
+              ),
+              SizedBox(height:20),
+              Row(
+                children:[
+                  Text(
+                    'Pick date : ',
+                    style: TextStyle(
+                      fontSize: 25,
+                      color: Color(0xFF001a33),
+                    ),
+                  ),
+                  SizedBox(width:10),
+                  ElevatedButton(
+                    onPressed: _showDatePicker,
                     style: ElevatedButton.styleFrom(
                       backgroundColor: Color(0xFF001a33),
                     ),
                     child: Text(
-                      _startTimeOfDay.format(context).toString(),
+                      _dateTime.month.toString()+'/'+
+                      _dateTime.day.toString()+'/'+
+                      _dateTime.year.toString(),
                       style: TextStyle(
                         fontSize: 25,
                       ),
                     ),
-                ),
-              ],
-            ),
-            SizedBox(height:30),
-            Row(
-              children:[
-                Text(
-                  'Pick Ending time :',
-                  style: TextStyle(
-                    fontSize: 25,
-                    color: Color(0xFF001a33),
                   ),
-                ),
-                SizedBox(width:10),
-                ElevatedButton(
-                  onPressed: _showTimePickerEnd,
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Color(0xFF001a33),
-                  ),
-                  child: Text(
-                    _endTimeOfDay.format(context).toString(),
+                ],
+              ),
+              SizedBox(height:20),
+              Row(
+                children:[
+                  Text(
+                    'Pick Starting time :',
                     style: TextStyle(
                       fontSize: 25,
+                      color: Color(0xFF001a33),
                     ),
                   ),
-                ),
-              ],
-            ),
-            Text(
-              '       (Allowed)',
-              style: TextStyle(
-                fontSize: 20,
-                color: Color(0xFF001a33),
+                  SizedBox(width:10),
+                  ElevatedButton(
+                      onPressed: _showTimePickerStart,
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xFF001a33),
+                      ),
+                      child: Text(
+                        _startTimeOfDay.format(context).toString(),
+                        style: TextStyle(
+                          fontSize: 25,
+                        ),
+                      ),
+                  ),
+                ],
               ),
-            ),
-            SizedBox(height:80),
-            Align(
-              alignment: Alignment.center,
-              child: MyButton(buttonText:'Start Class',onTap:(){
-                print(_startTimeOfDay);
-                print(_endTimeOfDay);
-                print(dropDownValue);
-                Navigator.pop(context);
-              })
-            )
-          ],
+              SizedBox(height:20),
+              Row(
+                children:[
+                  Text(
+                    'Pick Ending time :',
+                    style: TextStyle(
+                      fontSize: 25,
+                      color: Color(0xFF001a33),
+                    ),
+                  ),
+                  SizedBox(width:10),
+                  ElevatedButton(
+                    onPressed: _showTimePickerEnd,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xFF001a33),
+                    ),
+                    child: Text(
+                      _endTimeOfDay.format(context).toString(),
+                      style: TextStyle(
+                        fontSize: 25,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Text(
+                '       (Allowed)',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Color(0xFF001a33),
+                ),
+              ),
+              SizedBox(height:80),
+              Align(
+                alignment: Alignment.center,
+                child: MyButton(buttonText:'Start Class',onTap:(){
+                  Map<String,dynamic> data = {
+                    'course':dropDownValue,
+                    'day':_dateTime.day.toString(),
+                    'month':_dateTime.month.toString(),
+                    'start hour':_startTimeOfDay.hourOfPeriod,
+                    'start minute':_startTimeOfDay.minute,
+                    'start period':_startTimeOfDay.period.toString().substring(10),
+                    'end hour':_endTimeOfDay.hourOfPeriod,
+                    'end minute':_endTimeOfDay.minute,
+                    'end period':_endTimeOfDay.period.toString().substring(10),
+                  };
+                  sendData(data);
+                  Navigator.pop(context);
+                })
+              )
+            ],
+          ),
         ),
       ),
     );
   }
+}
+
+Future sendData(Map<String,dynamic> data) async{
+  final doc = FirebaseFirestore.instance.collection('classes');
+  await doc.add(data);
 }
