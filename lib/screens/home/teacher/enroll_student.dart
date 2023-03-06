71657122000780f6f469edae2426b4283562cc75
Farzana Reefat Raha <reefat.raha2018@gmail.com>
2023-03-06 18:45:40 +0600

    started enroll student

--- lib/screens/home/teacher/enroll_student.dart
+++ lib/screens/home/teacher/enroll_student.dart
@@ -1,4 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import 'get_student_info.dart';
 
 class enrollStudent extends StatefulWidget {
 
@@ -9,8 +14,48 @@ class enrollStudent extends StatefulWidget {
 }
 
 class _enrollStudentState extends State<enrollStudent> {
+
+  final user = FirebaseAuth.instance.currentUser!;
+
+  //document IDs
+  List<String> docIDs = [];
+  
+  //get docIDs
+  Future getDocId() async{
+    await FirebaseFirestore.instance.collection('users').get().then(
+        (snapshot) => snapshot.docs.forEach((document) {
+          print(document.reference);
+          docIDs.add(document.reference.id);
+        })
+    );
+  }
+
+  
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.blue.shade50,
+      body: FutureBuilder(
+              future: getDocId(),
+              builder: (context,snapshot) {
+                return ListView.builder(
+                    //itemCount: 5,
+                    itemBuilder: (BuildContext context, int index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ListTile(
+                        leading: CircleAvatar(
+                          backgroundColor: Color(0xFF001a33),
+                          child: Icon(Icons.people_alt_rounded),
+                        ),
+                        title: GetStudentInfo(documentId : docIDs[index]),
+                        tileColor: Colors.blue.shade100,
+                      ),
+                    );
+                },
+              );
+        }
+      ),
+    );
   }
 }
---
+++ lib/screens/home/teacher/get_student_info.dart
@@ -0,0 +1,36 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class GetStudentInfo extends StatelessWidget {
+  final String documentId;
+
+
+  const GetStudentInfo({required this.documentId});
+
+  @override
+  Widget build(BuildContext context) {
+
+    //get the Collection
+    CollectionReference users = FirebaseFirestore.instance.collection('users');
+    
+    return FutureBuilder<DocumentSnapshot>(
+      future: users.doc(documentId).get(),
+      builder: ((context,snapshot){
+        if(snapshot.connectionState == ConnectionState.done){
+            Map<String,dynamic> data = snapshot.data!.data() as Map<String,dynamic>;
+            return Text(
+              'role : ${data['role']}\nemail : ${data['email']}',
+              /*
+              style: TextStyle(
+                color: Colors.white,
+              ),
+
+               */
+            );
+
+        }
+        return Text('loading');
+      }),
+    );
+  }
+}
